6106401391b66e3f406d6520b79363fc08ec55e4
taftcreates <mistertaftcreates@gmail.com>
2019-01-04 20:52:20 -0700

    Update to newest video
    Renamed previous projects and uploaded most recent version of project.

---
+++ Part 35/Assets/Scripts/Objects/TreasureChest.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TreasureChest : Interactable {
+
+    public Item contents;
+    public Inventory playerInventory;
+    public bool isOpen;
+    public Signal raiseItem;
+    public GameObject dialogBox;
+    public Text dialogText;
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        {
+            if(!isOpen)
+            {
+                // Open the chest
+                OpenChest();
+            }else
+            {
+                // Chest is already open
+                ChestAlreadyOpen();
+            }
+        }
+    }
+
+    public void OpenChest()
+    {
+        // Dialog window on
+        dialogBox.SetActive(true);
+        // dialog text = contents text
+        dialogText.text = contents.itemDescription;
+        // add contents to the inventory
+        playerInventory.AddItem(contents);
+        playerInventory.currentItem = contents;
+        // Raise the signal to the player to animate
+        raiseItem.Raise();
+        // raise the context clue
+        context.Raise();
+        // set the chest to opened
+        isOpen = true;
+        anim.SetBool("opened", true);
+    }
+
+    public void ChestAlreadyOpen()
+    {
+        // Dialog off
+        dialogBox.SetActive(false);
+        // raise the signal to the player to stop animating
+        raiseItem.Raise();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger && !isOpen)
+        {
+            context.Raise();
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger && !isOpen)
+        {
+            context.Raise();
+            playerInRange = false;
+        }
+    }
+}
---
+++ Part 35/Assets/Scripts/ScriptableObjects/FloatValue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatValue : ScriptableObject, ISerializationCallbackReceiver {
+
+    public float initialValue;
+
+    [HideInInspector]
+    public float RuntimeValue;
+
+    public void OnAfterDeserialize(){
+        RuntimeValue = initialValue;
+    }
+
+    public void OnBeforeSerialize(){}
+}
